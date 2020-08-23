bd60dd32e655fdb19ff894ac2683060106be279d
serbuh <ser.buh@gmail.com>
2020-08-23 18:47:58 +0300

    still working

--- monitor_gui.py
+++ monitor_gui.py
@@ -23,24 +23,17 @@ class Window():
         #self.master.geometry('300x300')
         self.master.bind("<q>", self.quit_all)
 
-        #status_dict = {"Video FPS"  : 24.3,
-        #        "Telem FPS" : 22.0,
-        #        "frame"     : 23832,
-        #        "Last FPC"  : 13,
-        #        "FOV"       : (60.5, 55.6),
-        #        "Sensor"    : "VIS",
-        #        "CVS state" : "Scout"}
+        # Get first
         #self.add_labels(status_dict)
-        self.master.after(10, self.add_labels)
+        self.master.after(10, self.update_labels_text)
         
-    def add_labels(self):
-        print("Adding labels")
+    def update_labels_text(self):
         status_dict = self.conn.recv() # Get status_dict from UDP socket
 
         # Add labels in frames to the main window (root)
         max_name_len = max({len(x) for x in status_dict.keys()})
         max_value_len = max({len(str(status_dict[x])) for x in status_dict.keys()})
-        self.string_var_dict={}
+        self.curr_fields_dict = {} # Create / zeroing current fields list
         for row_count, field_name in enumerate(status_dict.keys()):
             # Frame
             frm_msg = tk.Frame(master=self.master,
@@ -54,10 +47,10 @@ class Window():
             frm_msg.grid(row=row_count, column=0, pady=0)
             
             # Label
-            self.string_var_dict[field_name] = tk.StringVar()             # Create new StringVar
-            self.string_var_dict[field_name].set(status_dict[field_name]) # Set text to the StringVar
+            self.curr_fields_dict[field_name] = tk.StringVar()             # Create new StringVar
+            self.curr_fields_dict[field_name].set(status_dict[field_name]) # Set text to the StringVar
             tk.Label(master = frm_msg, width=max_name_len, relief=tk.RAISED, bd=2, text = field_name).grid(row=0, column=0)
-            tk.Label(master = frm_msg, width=max_value_len, relief=tk.RAISED, bd=2, textvariable = self.string_var_dict[field_name]).grid(row=0, column=1) # bind to StringVar
+            tk.Label(master = frm_msg, width=max_value_len, relief=tk.RAISED, bd=2, textvariable = self.curr_fields_dict[field_name]).grid(row=0, column=1) # bind to StringVar
             
         print("Listening ...")
         self.master.after(10, self.refresh_labels)
@@ -66,20 +59,12 @@ class Window():
         status_dict = self.conn.recv() # Get status_dict from UDP socket
         for field_name in status_dict:
             try:
-                self.string_var_dict[field_name].set(status_dict[field_name]) # Set text to the StringVar
+                self.curr_fields_dict[field_name].set(status_dict[field_name]) # Set text to the StringVar
             except KeyError:
                 pass # received message with unknown label. Nothing to update
                 # Consider implementing online add_new_labels() function
         
         self.master.after(10, self.refresh_labels)
-
-    def add_new_labels(self):
-        raise(NotImplementedError)
-
-    def remove_labels(self):
-        raise(NotImplementedError)
-        
-
     def quit_all(self, event):
         print("pressed Q => Exit()")
         self.master.destroy()
